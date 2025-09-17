302504db630a343986ae5b696a87d42f97146e32
Riya agrawal <riyaagrawal2712@gmail.com>
2025-09-17 20:42:12 +0530

    Add files via upload

---
+++ averageOf3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter two numbers: ";
+    cin>>a>>b>>c;
+   int avg=(a+b+c)/3;
+   cout<<"average of 3= "<<avg;
+    return 0;
+}
