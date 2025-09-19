1b80e3118a27d628cd3928e28c92f1b338572bae
Riya agrawal <riyaagrawal2712@gmail.com>
2025-09-19 17:37:42 +0530

    Add files via upload

---
+++ areaOfTriangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float b,h;
+    cout<<"enter base and height: ";
+    cin>>b>>h;
+   
+   float area=b*h/2;
+   
+   cout<<"\narea of triangle= "<<area;
+    return 0;
+}
