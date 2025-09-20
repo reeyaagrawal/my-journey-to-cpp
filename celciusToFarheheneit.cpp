1e053112e16983b66177005c5cbe91e375e17bf2
Riya agrawal <riyaagrawal2712@gmail.com>
2025-09-20 22:29:04 +0530

    Add files via upload

---
+++ celciusToFarheheneit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float t;
+    cout<<"enter temperature in celcius: ";
+    cin>>t;
+   
+   float te=((9*t)/5)+32;
+   
+   cout<<"temperature in farehenheit= "<<te;
+    return 0;
+}
