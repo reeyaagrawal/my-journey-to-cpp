7f95a17e2af9f11a3af14c385bf27e82c68c9e1c
Riya agrawal <riyaagrawal2712@gmail.com>
2025-09-21 11:48:03 +0530

    Add files via upload

---
+++ circumferenceAndAreaOfCircle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<<"enter radius: ";
+    cin>>r;
+   float circum=2*3.14*r;
+   float area=3.14*r*r;
+   cout<<"circumference of circle= "<<circum;
+   cout<<"\narea of circle= "<<area;
+    return 0;
+}
