77d7a3fd658b7a787b8b397f4e7ac543103ccde0
Riya agrawal <riyaagrawal2712@gmail.com>
2025-09-23 16:02:28 +0530

    Add files via upload

---
+++ electricityBill.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter unit consumed: ";
+    cin>>a;
+    int b;
+    cout<<"enter rate per unit: ";
+    cin>>b;
+   int bill=a*b;
+   cout<<"electricity bill= "<<bill;
+    return o;
+}
