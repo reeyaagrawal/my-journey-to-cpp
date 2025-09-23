3e3d7418073a57ecd1934fc6f1571debaffa6103
Riya agrawal <riyaagrawal2712@gmail.com>
2025-09-23 16:02:42 +0530

    Add files via upload

---
+++ hoursToMinAndSec.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter no of hours: ";
+    cin>>a;
+    int min=a*60;
+    int sec=min*60;
+   cout<<"\n in minutes= "<<min;
+   
+   cout<<"\n in seconds= "<<sec;
+    return 0;
+}
+
