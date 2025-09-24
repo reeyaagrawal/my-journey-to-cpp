890e717828ce8a66efc2169a1f4c1880516038dd
Riya agrawal <riyaagrawal2712@gmail.com>
2025-09-24 10:28:16 +0530

    Add files via upload

---
+++ perimeterAndAreaOfRectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float l,b;
+    cout<<"enter two numbers: ";
+    cin>>l>>b;
+   float per=2*(l+b);
+   float area=l*b;
+   cout<<"perimeter of rectangle= "<<per;
+   cout<<"\narea of rectangle= "<<area;
+    return 0;
+}
