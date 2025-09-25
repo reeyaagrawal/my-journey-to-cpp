195932208b07e7187b0d08c32479525ac91753a5
Riya agrawal <riyaagrawal2712@gmail.com>
2025-09-25 17:01:43 +0530

    Add files via upload

---
+++ simpleInterest.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float a,b,c;
+    cout<<"enter two numbers: ";
+    cin>>a>>b>>c;
+   float si=(a*b*c)/100;
+   cout<<"SIMPLE INTEREST= "<<si;
+    return 0;
+}
