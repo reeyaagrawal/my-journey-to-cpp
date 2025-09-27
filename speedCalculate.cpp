f2005786e33f4c41bf7a68b90ae8a9bb03018901
Riya agrawal <riyaagrawal2712@gmail.com>
2025-09-27 18:51:23 +0530

    Add files via upload

---
+++ speedCalculate.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter distance: ";
+    cin>>a;
+    int b;
+    cout<<"enter time: ";
+    cin>>b;
+   cout<<"speed= "<<a/b;
+    return 0;
+}
