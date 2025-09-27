2cefa10cfd187b687c100c93bfe072d75b9e0a23
Riya agrawal <riyaagrawal2712@gmail.com>
2025-09-27 18:51:37 +0530

    Add files via upload

---
+++ squareAndCube.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter number: ";
+    cin>>a;
+   cout<<"square of no= "<<a*a;
+   cout<<"\ncube of no= "<<a*a*a;
+    return 0;
+}
