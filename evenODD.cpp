f8293eb3f4b9af367c6a684a9446324a6b615688
Riya agrawal <riyaagrawal2712@gmail.com>
2025-09-30 11:58:57 +0530

    Add files via upload

---
+++ evenODD.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number: ";
+    cin>>n;
+    if(n%2==0){
+        cout<<"Number is a even number.";
+    }else{
+        cout<<"number is a odd number.";
+    }
+    return 0;
+}
