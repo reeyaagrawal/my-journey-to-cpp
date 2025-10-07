0a7d9ef703d3f13fd9f93494b077e7fd2ca45ee7
Riya agrawal <riyaagrawal2712@gmail.com>
2025-10-07 15:21:45 +0530

    Add files via upload

---
+++ positiveOrNegativeNumber.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number: ";
+    cin>>n;
+    if(n>=0){
+        cout<<"Number is a positive number.";
+    }else{
+        cout<<"number is a negative number.";
+    }
+    return 0;
+}
