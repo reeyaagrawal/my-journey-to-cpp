4ecede63f6e29c9f6b321f1b1edc59c941df052d
Riya agrawal <riyaagrawal2712@gmail.com>
2025-10-17 20:49:39 +0530

    Add files via upload

---
+++ print100-1.cpp
@@ -0,0 +1,9 @@
+//  Write a program to print numbers from 100 down to 1.
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=100;i>=1;i--){
+        cout<<i<<" ";
+    }
+    return 0;
+}
