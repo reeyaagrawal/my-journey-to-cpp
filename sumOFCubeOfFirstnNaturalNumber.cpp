e405139fc6e3f40397f3447f19323e44bbcd737d
Riya agrawal <riyaagrawal2712@gmail.com>
2025-10-19 17:35:43 +0530

    Add files via upload

---
+++ sumOFCubeOfFirstnNaturalNumber.cpp
@@ -0,0 +1,14 @@
+// Write a program to calculate the sum of cubes of the first n natural numbers.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number: ";
+    cin>>n;
+    int sum=0;
+    for(int i=1;i<=n;i++){
+       sum+=i*i*i;
+        }
+  cout<<"sum of cubes of the first "<< n<<" natural numbers: "<<sum;
+    return 0;
+}
