be20900f1d3fe7674cfca6c22077f6b663c92115
Riya agrawal <riyaagrawal2712@gmail.com>
2025-10-20 12:52:24 +0530

    Add files via upload

---
+++ sumOfEvenNumberBetween1-100.cpp
@@ -0,0 +1,13 @@
+//Write a program to find the sum of all even numbers between 1 and 100.
+#include<iostream>
+using namespace std;
+int main(){
+    int sum=0;
+    for(int i=1;i<=100;i++){
+        if(i%2==0){
+        sum+=i;
+        }
+    }
+    cout<<"sum of all even number b/w 1 and 100= "<<sum;
+    return 0;
+}
