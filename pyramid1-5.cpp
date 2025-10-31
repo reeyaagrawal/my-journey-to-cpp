469b2699958469c384cfe098f10ca4753182bf3e
Riya agrawal <riyaagrawal2712@gmail.com>
2025-10-31 22:19:52 +0530

    Add files via upload

---
+++ pyramid1-5.cpp
@@ -0,0 +1,12 @@
+// Write a program to print a right-angled triangle of numbers.
+#include <iostream>
+using namespace std;
+int main() {
+  for(int i=1;i<=5;i++){
+      for(int j=1;j<=i;j++){
+          cout<<j;
+       }
+      cout<<endl;
+  }
+    return 0;
+}
