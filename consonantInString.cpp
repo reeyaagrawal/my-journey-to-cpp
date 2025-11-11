f79cc629cc5f3e71c25768a7c783a103cc0f3efa
Riya agrawal <riyaagrawal2712@gmail.com>
2025-11-11 19:35:38 +0530

    Add files via upload

---
+++ consonantInString.cpp
@@ -0,0 +1,17 @@
+// Write a program to count the number of consonants in a string.
+#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+    string str;
+    cout<<"enter string:";
+    getline(cin,str);
+    int count=0;
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]!='a' && str[i]!='e' && str[i]!='i' && str[i]!='o' && str[i]!='u' && str[i]!='A' && str[i]!='E' && str[i]!='I' && str[i]!='O'){
+            count++;
+        }
+    }
+cout<<"no of consonant in string are: "<<count;
+    return 0;
+}
